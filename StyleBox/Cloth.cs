f0f27a90db12d77a6fe74c3d0bb23a5a3b2b3b68
Andrii Chekmarov <andrei.tchekmarou@gmail.com>
2025-05-07 17:50:44 +0300

    как

---
+++ StyleBox/Cloth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyleBox
+{
+    public class GlobalClass
+    {
+        public struct Cloth
+        {
+            public string cloth_article;
+            public string cloth_name;
+            public string cloth_type;
+            public double cloth_price;
+            public int cloth_number;
+
+            public Cloth(string article, string name, string type, double price, int number)
+            {
+                this.cloth_article = article;
+                cloth_name = name;
+                cloth_type = type;
+                cloth_price = price;
+                cloth_number = number;
+            }
+        }
+
+
+    }
+}
--- StyleBox/MainWindow.xaml.cs
+++ StyleBox/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+using MySql.Data.MySqlClient;
+using static StyleBox.GlobalClass;
+
+
 namespace StyleBox
 {
     /// <summary>
@@ -51,6 +56,48 @@ namespace StyleBox
             EditForm editForm = new EditForm();
             editForm.Show();
             Hide();
+        }
+
+        private void Db_download(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connstring = "server=localhost;uid=root;pwd=135135135;database=stylebox_db";
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+                string sql = "select * from stocks";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                //List<GlobalClass.Cloth> clothlist = new List<GlobalClass.Cloth>();
+                GlobalClass.Cloth[] clothList = new GlobalClass.Cloth[10];
+
+                int i = 0;
+                while (reader.Read())
+                {
+                    //string name = reader["cloth_name"];
+                    //MessageBox.Show("typeof name" + reader["cloth_name"] + )
+
+                    //ClothDataGrid.Items.Add(reader["cloth_article"] + " " + reader["cloth_name"]);
+                    //clothlist.Add(new Cloth(reader["cloth_articel"], reader["cloth_name", "M", Convert.ToDouble(reader["cloth_price"]), (int)reader["cloth_number"]);
+                    clothList[i].cloth_article = (string)reader["cloth_article"];
+                    clothList[i].cloth_name = (string)reader["cloth_name"];
+                    clothList[i].cloth_type = (string)reader["cloth_type"];
+                    clothList[i].cloth_price = Convert.ToDouble(reader["cloth_price"]);
+                    clothList[i].cloth_number = Convert.ToInt32(reader["cloth_number"]);
+                    i++;
+
+                    
+                }
+
+                ClothListDG.ItemsSource = clothList;
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
         }
     }
 }
